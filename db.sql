58b56b159c6a0ba099d039df0c7eb3ecab6f698e
Thomas <105030215+iKeiNagai@users.noreply.github.com>
2025-04-29 14:26:30 -0400

    added users table

---
+++ db.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS lifegame;
+CREATE DATABASE lifegame;
+
+USE lifegame;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
