c31740b7e90c175c756dc41c26e5f66f0ece432b
Thomas <105030215+iKeiNagai@users.noreply.github.com>
2025-05-01 20:24:36 -0400

    game sessions table

--- db.sql
+++ db.sql
@@ -9,3 +9,14 @@ CREATE TABLE users (
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE game_sessions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pattern_name VARCHAR(100),
+    status ENUM('died', 'succeeded'),
+    generation_count INT,
+    created_at VARCHAR(30),
+    ended_at VARCHAR(30),
+    user_id INT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
